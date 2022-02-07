f769605a8b3b971f6f0caaa03ca4e8cd8221f633
Joe <kerrjo@me.com>
2022-02-07 09:11:23 -0500

    Initial Commit

---
+++ UserList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserList
+//
+//  Created by JOSEPH KERR on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by JOSEPH KERR on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
